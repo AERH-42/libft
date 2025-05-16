6b35745b2a4b75c1b57c332d0e9547ff64b7f3df
Alvan Zhi Huang Erh <aerh@c2r1s13.42singapore.sg>
2025-05-16 13:50:06 +0800

    amended split

--- ft_split.c
+++ ft_split.c
@@ -6,18 +6,19 @@
 /*   By: aerh <aerh@student.42singapore.sg>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/09 16:10:16 by aerh              #+#    #+#             */
-/*   Updated: 2025/04/10 11:01:10 by aerh             ###   ########.fr       */
+/*   Updated: 2025/05/16 13:49:44 by aerh             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+#include <stdio.h>
 
-static size_t	find_seperator(char a, char c)
-{
-	if (c == a)
-		return (1);
-	return (0);
-}
+// static size_t	find_seperator(char a, char c)
+// {
+// 	if (c == a)
+// 		return (1);
+// 	return (0);
+// }
 
 static size_t	count_words(char const *str, char c)
 {
@@ -30,7 +31,7 @@ static size_t	count_words(char const *str, char c)
 	word = 0;
 	while (str[i] != '\0')
 	{
-		if (find_seperator(str[i], c))
+		if (str[i] == c)
 			word = 0;
 		else if (!word)
 		{
@@ -42,59 +43,48 @@ static size_t	count_words(char const *str, char c)
 	return (count);
 }
 
-static char	*ft_strmal(char const *src, size_t size)
-{
-	size_t	i;
-	char	*dest;
-
-	if (src == NULL)
-		return (NULL);
-	dest = (char *)malloc((size + 1) * sizeof(char));
-	if (dest == NULL)
-		return (NULL);
-	i = 0;
-	while (src[i] != '\0' && i < size)
-	{
-		dest[i] = src[i];
-		i++;
-	}
-	dest[i] = 0;
-	return (dest);
-}
+// static char	*ft_putword(char **arr, char const *str, char c)
+// {
+// 	size_t	i;
+// 	size_t	start;
+// 	size_t	end;
+// 	size_t	word;
+// }
 
 char	**ft_split(char const *str, char c)
 {
 	char	**arr;
 	size_t	i;
-	size_t	x;
-	size_t	k;
+	size_t	start;
+	size_t	end;
+	size_t	word;
 
-	if (str == NULL || &c == NULL)
+	if (str == NULL)
 		return (NULL);
 	arr = malloc((count_words(str, c) + 1) * sizeof(char *));
 	if (arr == NULL)
 		return (NULL);
 	i = 0;
-	k = 0;
-	while (str[i] != '\0' && k < count_words(str, c))
+	word = 0;
+	while (str[i] != '\0' && word < count_words(str, c))
 	{
-		while (str[i] != '\0' && find_seperator(str[i], c))
+		start = 0;
+		end = 0;
+		while (str[i] == c && str[i] != '\0')
 			i++;
-		x = i;
-		while (str[i] != '\0' && !find_seperator(str[i], c))
+		start += i;
+		while (str[i] != c && str[i] != '\0')
 			i++;
-		if (i > x)
-			arr[k++] = ft_strmal(&str[x], i - x);
+		end += i;
+		arr[word++] = ft_substr(str, start, end - start);
 	}
-	arr[k] = 0;
+	arr[word] = 0;
 	return (arr);
 }
 
-#include <stdio.h>
-
 int main() 
 {
-    char *str = "hello,world,how,are,you";
+    char str[] = ",hello,world";
     char charset = ',';
     char **result = ft_split(str, charset);  
     printf("Basic split: %s\n", str);
