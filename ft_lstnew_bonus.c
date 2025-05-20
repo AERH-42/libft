6b76a1af4b62405e8bf48ca9c02e0e70e4cb6c0e
Alvan Zhi Huang Erh <aerh@c2r2s11.42singapore.sg>
2025-05-20 21:52:31 +0800

    adding lstnew for bonus

--- ft_lstnew_bonus.c
+++ ft_lstnew_bonus.c
@@ -6,16 +6,23 @@
 /*   By: aerh <aerh@student.42singapore.sg>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/02 12:37:49 by aerh              #+#    #+#             */
-/*   Updated: 2025/05/20 17:54:32 by aerh             ###   ########.fr       */
+/*   Updated: 2025/05/20 21:51:15 by aerh             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-t_list	*ft_lstnew(void *content);
+t_list	*ft_lstnew(void *content)
 {
-	
+	t_list	*node;
+
+	node = malloc(sizeof(t_list));
+	if (!node)
+		return (NULL);
+	node->content = content;
+	node->next = NULL;
 }
+
 
 // int main(void)
 // {
