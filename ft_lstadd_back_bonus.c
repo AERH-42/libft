1b52ca756d11c7b09cee2061899ae1c85efd2159
Alvan Zhi Huang Erh <aerh@c2r1s13.42singapore.sg>
2025-05-22 15:27:29 +0800

    working lstadd_back

---
+++ ft_lstadd_back_bonus.c
@@ -0,0 +1,59 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstadd_back_bonus.c                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aerh <aerh@student.42singapore.sg>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/04/02 12:37:49 by aerh              #+#    #+#             */
+/*   Updated: 2025/05/22 15:26:48 by aerh             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	ft_lstadd_back(t_list **lst, t_list *new)
+{
+	t_list	*last;
+
+	if (new == NULL)
+		return ;
+	if (*lst == NULL)
+		*lst = new;
+	else
+	{
+		last = ft_lstlast(*lst);
+		last->next = new;
+	}
+	new->next = NULL;
+}
+
+// #include <stdio.h>
+
+// int main(void)
+// {
+// 	t_list	*lst = NULL;
+// 	t_list	*node1 = ft_lstnew("Hello");
+// 	t_list	*node2 = ft_lstnew("World");
+// 	ft_lstadd_back(&lst, node1);
+// 	ft_lstadd_front(&lst, node2);
+// 	t_list *tmp = lst;
+// 	while (tmp)
+// 	{
+// 		printf ("%s\n", (char *)tmp->content);
+// 		tmp = tmp->next;
+// 	}
+// 	while (lst)
+// 	{
+// 		t_list *next = lst->next;
+// 		free(lst);
+// 		lst = next;
+// 	}
+// }
+
+/*
+Check for empty list. If *lst is NULL, make new the first node
+Find current last node, if not empty list, using ft_lstlast
+Link new node by setting next pointer of last node to point to new
+Set next node of new to NULL
+*/
